e99305e2e7eb9d6389664107d92a0fab1d5c078e
Diego Dauri <dauridiego@gmail.com>
2021-07-18 13:26:44 +0200

    Finish application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,23 @@ class UsersController < ApplicationController
   
   def my_portfolio
     @tracked_stocks = current_user.stocks
+    @user = current_user
   end
 
   def my_friends
     @friends = current_user.friends
   end
+
+  def show 
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
   
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      @friends =  current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js { render partial: "users/friend_result" }
         end
@@ -23,7 +30,7 @@ class UsersController < ApplicationController
       end
     else
       respond_to do |format|
-        flash.now[:alert] = "Please enter a friend name or email to"
+        flash.now[:alert] = "Please enter a friend name or email to search"
         format.js { render partial: "users/friend_result" }
       end
     end
